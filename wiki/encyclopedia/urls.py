4676528bfa678d80c3151192a38aaade4db6bdde
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-01-09 15:17:13 +0300

    update

---
+++ wiki/encyclopedia/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+# app_name = "encyclopedia"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/", views.wiki, name="wiki"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page"),
+    path("save_content/", views.save_content, name="save_content"),
+]
---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,80 @@
+import markdown
+from django import forms
+from django.shortcuts import render
+
+from . import util
+
+
+def convert_md(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+
+def index(request):
+    return render(
+        request,
+        "encyclopedia/index.html",
+        {"entries": util.list_entries()},
+    )
+
+
+def wiki(request, title):
+    html_content = convert_md(title)
+    if html_content == None:
+        return render(
+            request, "encyclopedia/error.html", {"message": "Content Not Found"}
+        )
+    else:
+        return render(
+            request,
+            "encyclopedia/entry_page.html",
+            {
+                "title": title,
+                "content": html_content,
+            },
+        )
+
+
+def search(request):
+    if request.method == "POST":
+        search_request = request.POST["q"]
+        html_content = convert_md(search_request)
+        if html_content is not None:
+            return render(
+                request,
+                "encyclopedia/entry_page.html",
+                {"title": search_request, "content": html_content},
+            )
+        else:
+            return render(
+                request, "encyclopedia/error.html", {"message": "content not found"}
+            )
+
+
+def new_page(request):
+    return render(request, "encyclopedia/new_page.html")
+
+
+def save_content(request):
+    if request.method == "POST":
+        content = request.POST["content"]
+        title = request.POST["title"].lower()
+        check_title = util.list_entries()
+        lowercase_list = [item.lower() for item in check_title]
+        if title in lowercase_list:
+            return render(
+                request,
+                "encyclopedia/error.html",
+                {"message": "content already exists"},
+            )
+        else:
+            util.save_entry(title, content)
+            return render(
+                request,
+                "encyclopedia/entry_page.html",
+                {"title": title, "content": content},
+            )
