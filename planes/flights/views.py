3853dd0d3d9e425a9aef3cd440479727f6db7a10
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-01-16 11:43:04 +0300

    update

--- planes/flights/views.py
+++ planes/flights/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passengers
 
 
 # Create your views here.
@@ -21,5 +23,15 @@ def flight(request, flight_id):
         "flights/flight.html",
         {
             "flight": flight,
+            "passengers": flight.passengers.all(),
+            "non_passengers": Passengers.objects.exclude(flights=flight).all(),
         },
     )
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passengers.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
